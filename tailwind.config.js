7fb87b3922940dd241f7505c10db8525c0bb3a5f
Ignacio F Mandagaran <imandagaran@makingsense.com>
2021-04-27 09:54:42 -0300

    change bg-image hide/show approach

--- tailwind.config.js
+++ tailwind.config.js
@@ -917,7 +917,7 @@ module.exports = {
     mixBlendMode: ['responsive'],
     objectFit: ['responsive'],
     objectPosition: ['responsive'],
-    opacity: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus'],
+    opacity: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus', 'dark'],
     order: ['responsive'],
     outline: ['responsive', 'focus-within', 'focus'],
     overflow: ['responsive'],
