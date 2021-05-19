2cf6caf947668196bfb4747765933427ef0a675f
Ignacio F Mandagaran <imandagaran@makingsense.com>
2021-05-18 22:25:31 -0300

    New animation

--- src/app/components/orbits-animation/orbits-animation.component.ts
+++ src/app/components/orbits-animation/orbits-animation.component.ts
@@ -1,16 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-orbits-animation',
   templateUrl: './orbits-animation.component.html',
-  styleUrls: ['./orbits-animation.component.scss']
+  styleUrls: ['./orbits-animation.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class OrbitsAnimationComponent implements OnInit {
 
   @Input()
   darkmode = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {}
 
